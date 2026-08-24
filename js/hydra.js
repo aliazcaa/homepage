2184b7150f10f5d863cdf42ad303ae58fe4beb2d
Ali Azca <89765186+aliazcaa@users.noreply.github.com>
2026-08-25 02:05:51 +0700

    hydra.js

--- js/hydra.js
+++ js/hydra.js
@@ -1,22 +1,39 @@
-speed = 0.1
+speed = 1
 
-shape(100)
-  .scale(1,1,1.78)
+s0.initCam()
+
+src(s0)
+  //.scale(1,1,1.78)
   .out(o0)
 
 src(o0)
-  .modulateRotate(osc(()=>-mouse.x / width),()=> -mouse.y / height)
-  .modulateScrollX(src(o1),()=>-mouse.x / width,0.2)
-  .modulateScrollY(src(o2),()=> -mouse.y / height,0.5)
+  .diff(shape([1,100].ease('easeInOutSine').smooth()))
+  .scale(1,1,1.78)
+  .modulatePixelate(src(o3),[4,4000].ease('easeInOutCubic'))
+  .modulateScale(src(o1),()=>Math.sin(time)*0.5,0.5)
+  .modulateScrollY(src(o2),()=>Math.sin(time),-0.5)
   .thresh().invert()
   .out(o1)
 
 shape(4)
   .scale(1,1,1.78)
-  .modulateScale(src(o2))
-  .modulateRepeat(src(o1),4,2)
+  .modulateRepeat(src(o1),[1,2,3,5,7]
+                  //.ease('easeInOutSine').smooth(),[2,4,6,8,10]
+                  //.ease('easeInOutCubic').smooth()
+                 )
+  .modulateScale(src(o3),[10,8,4,2,1].fast(2),1,1.78)
   .thresh()
   .invert()
+  .scale([0.3,1,0.9,0.4,0.5,0.6].fast())
+  .invert()
   .out(o2)
+
+src(o2)
+  //.modulateKaleid(src(o2))
+  //.blend(src(s0))
+  .modulateScrollX(src(o1),0.5,()=>Math.sin(time)*0.5)
+  .modulateScrollY(src(o2),0.5,()=>Math.sin(time)*-0.5)
+  .modulate(src(o3))
+  .out(o3)
 
 render(o2)
